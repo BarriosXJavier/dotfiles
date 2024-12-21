fe895c898d69b9ffd5581b374b18534c5996fa56
David Muriithi <muriithid05@gmail.com>
2024-12-21 23:24:45 +0300

    modified:   .tmux.conf 	modified:   nvim/lazy-lock.json 	modified:   nvim/lua/plugins/init.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -222,4 +222,24 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
   },
+
+  { "anuvyklack/hydra.nvim" },
+
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvimtools/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+    },
+  },
 }
