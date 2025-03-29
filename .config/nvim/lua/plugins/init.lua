41e396777e5bca25d480dba856e4048612bf1d77
David Muriithi <muriithid05@gmail.com>
2025-03-29 13:36:31 +0300

    modified:   .config/nvim/lazy-lock.json 	modified:   .config/nvim/lua/plugins/init.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,14 +1,5 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre", "BufNewFile", "LspAttach", "BufReadPost" },
-    opts = function()
-      local config = require "configs.conform"
-      config.format_on_save = { timeout_ms = 500, lsp_fallback = true, async = true }
-      return config
-    end,
-  },
-
+  -- Core plugins
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -17,33 +8,24 @@ return {
   },
 
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-      },
-    },
+    "williamboman/mason-lspconfig.nvim",
   },
 
-  { "wakatime/vim-wakatime", lazy = false },
-
-  --[[{
-    "Exafunction/codeium.vim",
-    event = "BufEnter",
-  },
-  ]]
   {
-    "ellisonleao/carbon-now.nvim",
-    lazy = true,
-    cmd = "CarbonNow",
-    opts = { --[[ your custom config here ]]
-    },
+    "stevearc/conform.nvim",
+    event = { "BufWritePre", "BufNewFile", "LspAttach", "BufReadPost" },
+    opts = function()
+      local config = require "configs.conform"
+      config.format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+        async = true,
+      }
+      return config
+    end,
   },
 
+  -- UI enhancements
   {
     "NvChad/ui",
     config = function()
@@ -60,34 +42,8 @@ return {
   },
 
   {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-  },
-
-  { "williamboman/mason-lspconfig.nvim" },
-
-  { "mfussenegger/nvim-dap" },
-
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^4",
-    lazy = false,
-  },
-
-  {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
-
-  { "nvim-tree/nvim-web-devicons" },
-
-  {
-    "Pocco81/auto-save.nvim",
-    config = function()
-      require("auto-save").setup {}
-    end,
-    lazy = false,
   },
 
   {
@@ -96,67 +52,38 @@ return {
     lazy = false,
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
-      require("bufferline").setup {}
+      require("bufferline").setup()
     end,
   },
 
   {
-    "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = "Trouble",
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
       },
     },
   },
 
+  -- Editing helpers
   {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup()
+    end,
+  },
+
+  {
+    "Pocco81/auto-save.nvim",
+    lazy = false,
+    config = function()
+      require("auto-save").setup()
+    end,
   },
 
   {
@@ -167,27 +94,15 @@ return {
   },
 
   {
-    "jinzhongjia/LspUI.nvim",
-    branch = "main",
+    "BarriosXJavier/tag-replacer.nvim",
+    lazy = false,
+    cmd = { "ReplaceTag", "ReplaceTagVisual" },
     config = function()
-      require("LspUI").setup {
-        -- config options go here
-      }
+      require("tag-replacer").setup()
     end,
   },
 
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = true,
-    opts = {
-      direction = "float",
-    },
-    keys = {
-      { "<leader>tt", "<cmd>ToggleTerm<CR>", desc = "Toggle terminal" },
-    },
-  },
-
+  -- LSP enhancements
   {
     "nvimdev/lspsaga.nvim",
     event = "LspAttach",
@@ -205,204 +120,133 @@ return {
     },
   },
 
-  -- [[{
-  --"zbirenbaum/copilot.lua",
-  --cmd = "Copilot",
-  --event = "InsertEnter",
-  --config = function()
-  --require("copilot").setup {}
-  --end,
-  -- }]],
-
-  -- {
-  --   "zbirenbaum/copilot-cmp",
-  --   config = function()
-  --     require("copilot_cmp").setup()
-  --   end,
-  -- },
-
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   dependencies = {
-  --     -- -- GitHub Copilot integration
-  --     -- {
-  --     --   "github/copilot.vim", -- Base Copilot plugin
-  --     -- },
-  --     -- {
-  --     --   "zbirenbaum/copilot-cmp", -- CMP source for Copilot
-  --     --   config = function()
-  --     --     require("copilot_cmp").setup()
-  --     --   end,
-  --     -- },
-  --
-  --     -- Command line completion
-  --     {
-  --       "hrsh7th/cmp-cmdline",
-  --       event = "CmdlineEnter",
-  --       config = function()
-  --         local cmp = require "cmp"
-  --         cmp.setup.cmdline("/", {
-  --           mapping = cmp.mapping.preset.cmdline(),
-  --           sources = { { name = "buffer" } },
-  --         })
-  --
-  --         cmp.setup.cmdline(":", {
-  --           mapping = cmp.mapping.preset.cmdline(),
-  --           sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
-  --           matching = { disallow_symbol_nonprefix_matching = false },
-  --         })
-  --       end,
-  --     },
-  --   },
-  --
-  --   opts = function(_, opts)
-  --     -- Add Copilot as a completion source
-  --     table.insert(opts.sources, 1, {
-  --       name = "copilot",
-  --       group_index = 1,
-  --       priority = 100,
-  --     })
-  --
-  --     -- Optional: Configure trigger characters if needed
-  --     opts.sources[1].trigger_characters = { "-" }
-  --   end,
-  -- },
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
+    "jinzhongjia/LspUI.nvim",
+    branch = "main",
     config = function()
-      require("copilot").setup {}
+      require("LspUI").setup()
     end,
   },
 
+  -- Debugging
   {
-    "zbirenbaum/copilot-cmp",
-    config = function()
-      require("copilot_cmp").setup()
-    end,
+    "mfussenegger/nvim-dap",
   },
 
+  -- Rust specific
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    lazy = false,
+  },
+
+  -- Productivity tools
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+
+  {
+    "ellisonleao/carbon-now.nvim",
+    lazy = true,
+    cmd = "CarbonNow",
+    opts = {},
+  },
+
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    opts = {
+      direction = "float",
+    },
+    keys = {
+      { "<leader>tt", "<cmd>ToggleTerm<CR>", desc = "Toggle terminal" },
+    },
+  },
+
+  -- Diagnostics
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = {},
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+    },
+  },
+
+  -- Completion (Updated nvim-cmp config)
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      -- GitHub Copilot integration
-      {
-        "github/copilot.vim", -- Base Copilot plugin
-      },
-      {
-        "zbirenbaum/copilot-cmp", -- CMP source for Copilot
-        config = function()
-          require("copilot_cmp").setup()
-        end,
-      },
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+    },
+    config = function()
+      local cmp = require "cmp"
+      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+      cmp.setup {
+        mapping = cmp.mapping.preset.insert {
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm { select = true }, -- Proper Enter key mapping
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        },
+        sources = cmp.config.sources {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      }
 
       -- Command line completion
-      {
-        "hrsh7th/cmp-cmdline",
-        event = "CmdlineEnter",
-        config = function()
-          local cmp = require "cmp"
-          cmp.setup.cmdline("/", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = { { name = "buffer" } },
-          })
-
-          cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
-            matching = { disallow_symbol_nonprefix_matching = false },
-          })
-        end,
-      },
-    },
-
-    opts = function(_, opts)
-      -- Add Copilot as a completion source
-      table.insert(opts.sources, 1, {
-        name = "copilot",
-        group_index = 1,
-        priority = 100,
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = "buffer" } },
       })
 
-      -- Optional: Configure trigger characters if needed
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+        matching = { disallow_symbol_nonprefix_matching = false },
+      })
+    end,
+    opts = function(_, opts)
       opts.sources[1].trigger_characters = { "-" }
     end,
   },
 
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-    },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for options
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
-
-  {
-    "BarriosXJavier/tag-replacer.nvim",
-    lazy = false,
-    cmd = { "ReplaceTag", "ReplaceTagVisual" },
-    config = function()
-      require("tag-replacer").setup()
-    end,
-  },
-
-  {
-    "saghen/blink.cmp",
-    -- optional: provides snippets for the snippet source
-    dependencies = { "rafamadriz/friendly-snippets" },
-
-    -- use a release tag to download pre-built binaries
-    version = "1.*",
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = "default" },
-
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = "mono",
-      },
-
-      -- (Default) Only show the documentation popup when manually triggered
-      completion = { documentation = { auto_show = false } },
-
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
-
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "prefer_rust_with_warning" },
-    },
-    opts_extend = { "sources.default" },
-  },
+  -- Misc
+  { "wakatime/vim-wakatime", lazy = false },
+  { "nvim-tree/nvim-web-devicons" },
 }
