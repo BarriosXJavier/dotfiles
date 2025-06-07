68938b24b4db7fc0dc19da2af00c357802966eac
David Muriithi <muriithid05@gmail.com>
2025-06-07 16:23:44 +0300

    switch to Pocco81/autosave

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -137,11 +137,13 @@ return {
   },
 
   {
-    "okuuva/auto-save.nvim",
-    version = "^1.0.0",
-    cmd = "ASToggle",
-    event = { "InsertLeave", "TextChanged", "BufLeave" },
-    opts = {},
+    "Pocco81/auto-save.nvim",
+    config = function()
+      require("auto-save").setup {
+        -- your config goes here
+        -- or just leave it empty :)
+      }
+    end,
   },
 
   {
