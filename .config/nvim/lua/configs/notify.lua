e0c28460358fb75d3c611b3ee1fad22bc85b2261
David Muriithi <muriithid05@gmail.com>
2025-06-12 10:25:08 +0300

    adjust the notify's notification box size

--- .config/nvim/lua/configs/notify.lua
+++ .config/nvim/lua/configs/notify.lua
@@ -2,9 +2,11 @@ local M = {}
 
 function M.setup()
   require("notify").setup {
-    render = "compact",
+    render = "wrapped-compact",
     background_colour = "#1e1e2e",
-    max_width = 35,
+    max_width = function()
+      return math.floor(vim.o.columns * 0.5)
+    end,
     stages = "slide",
     timeout = 2500,
     top_down = false,
