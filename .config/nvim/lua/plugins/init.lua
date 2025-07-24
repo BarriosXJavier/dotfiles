beff1921898a1da7dfd2654201b3c810cda55205
David Muriithi <muriithid05@gmail.com>
2025-07-24 18:48:06 +0300

    add dadbob for dbs

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -25,6 +25,24 @@ return {
   },
 
   {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+
+  {
     "okuuva/auto-save.nvim",
     version = "*",
     cmd = "ASToggle",
@@ -45,6 +63,8 @@ return {
       },
     },
   },
+
+  { "tpope/vim-dadbod" },
 
   {
     "jiaoshijie/undotree",
