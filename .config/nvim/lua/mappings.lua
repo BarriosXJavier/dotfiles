eaa673a901aa1ef3f8a0b7fed55fe6006988c1bf
David Muriithi <muriithid05@gmail.com>
2025-07-30 22:29:38 +0300

    scroll mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,17 +1,20 @@
-require "nvchad.mappings"
-
--- add yours here
+require("nvchad.mappings")
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- Normal mode for faster command mode
+map("n", ";", ":", { desc = "CMD: Enter command mode" })
 
-map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+-- jk to escape insert mode
+map("i", "jk", "<ESC>", { desc = "Esc: Escape insert mode" })
 
--- resize with arrows
-map("n", "<C-Left>", ":vertical resize -2<CR>", nore)
-map("n", "<C-Right>", ":vertical resize +2<CR>", nore)
+-- Toggle nvim tree
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle Nvim Tree" })
 
+-- Resize splits with arrow keys
+map("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, desc = "Resize split: Shrink" })
+map("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, desc = "Resize split: Expand" })
 
-
+-- Scroll down/up and center the screen
+map("n", "<C-d>", "<C-d>zz", { noremap = true, desc = "Scroll down and center" })
+map("n", "<C-u>", "<C-u>zz", { noremap = true, desc = "Scroll up and center" })
