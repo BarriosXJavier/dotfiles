f1c851c21a6851e181bc7246e8470056bd545d49
David Muriithi <muriithid05@gmail.com>
2025-12-18 23:38:39 +0300

    emacs

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,194 @@
+(tool-bar-mode -1)             ; Hide the outdated icons
+(scroll-bar-mode -1)           ; Hide the always-visible scrollbar
+(menu-bar-mode -1)             ; Hide the menu bar
+(setq inhibit-splash-screen t) ; Remove the "Welcome to GNU Emacs" splash screen
+(setq use-file-dialog nil)     ; Ask for textual confirmation instead of GUI
+(setq frame-resize-pixelwise t) ; Allow pixel-level resizing
+(add-to-list 'default-frame-alist '(undecorated . t)) ; Remove title bar
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+    (expand-file-name
+      "straight/repos/straight.el/bootstrap.el"
+      (or (bound-and-true-p straight-base-dir)
+        user-emacs-directory)))
+    (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+       'silent 'inhibit-cookies)
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Syntax: (set-frame-font "FONT-NAME-SIZE" KEEP-SIZE ALL-FRAMES)
+(set-frame-font "Iosevka Nerd Font-12" nil t)
+
+(setq package-enable-at-startup nil)
+
+(straight-use-package 'use-package)
+
+(setq straight-use-package-by-default t)
+(setq straight-check-for-modifications '(check-on-save find-when-checking))
+(setq use-package-always-defer t)
+
+(use-package emacs
+  :init
+  (setq initial-scratch-message nil)
+  (defun display-startup-echo-area-message ()
+    (message "")))
+
+(use-package emacs
+  :init
+  (defalias 'yes-or-no-p 'y-or-n-p))
+
+(use-package emacs
+  :init
+  (set-charset-priority 'unicode)
+  (setq locale-coding-system 'utf-8
+        coding-system-for-read 'utf-8
+        coding-system-for-write 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (set-selection-coding-system 'utf-8)
+  (prefer-coding-system 'utf-8)
+  (setq default-process-coding-system '(utf-8-unix . utf-8-unix)))
+
+(use-package emacs
+  :init
+  (setq-default indent-tabs-mode nil)
+  (setq-default tab-width 2)
+  ;; Better RET behavior: indent new line automatically
+  (electric-indent-mode 1)
+  ;; Disable backup files (like Vim's .swp files)
+  (setq make-backup-files nil
+        auto-save-default nil
+        create-lockfiles nil)
+  ;; Better scrolling
+  (setq scroll-margin 3
+        scroll-conservatively 101
+        scroll-preserve-screen-position t)
+  ;; Smooth mouse scrolling
+  (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))
+        mouse-wheel-progressive-speed nil))
+
+
+(use-package doom-themes
+  :demand
+  :config
+  (load-theme 'doom-challenger-deep t))
+
+(use-package emacs
+  :init
+  (defun ab/enable-line-numbers ()
+    "Enable relative line numbers"
+    (interactive)
+    (display-line-numbers-mode)
+    (setq display-line-numbers 'relative))
+  (add-hook 'prog-mode-hook #'ab/enable-line-numbers))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package nerd-icons)
+
+;; which-key: shows available keybindings in popup
+(use-package which-key
+  :demand
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 0.5
+        which-key-popup-type 'side-window
+        which-key-side-window-location 'bottom
+        which-key-side-window-max-height 0.25))
+
+;; Magit: Git interface
+(use-package magit
+  :bind ("C-x g" . magit-status))
+
+;; Projectile: Project management
+(use-package projectile
+  :demand
+  :bind-keymap ("C-c p" . projectile-command-map)
+  :config
+  (projectile-mode +1)
+  (setq projectile-project-search-path '("~/projects" "~/dotfiles")))
+
+;; Ivy/Counsel: Fuzzy finding and completion
+(use-package ivy
+  :demand
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t
+        ivy-count-format "(%d/%d) "
+        enable-recursive-minibuffers t))
+
+(use-package counsel
+  :demand
+  :bind (("C-s" . swiper)
+         ("C-x C-f" . counsel-find-file)
+         ("C-x b" . counsel-switch-buffer)
+         ("M-x" . counsel-M-x))
+  :config
+  (counsel-mode 1))
+
+(use-package ivy-rich
+  :after ivy
+  :demand
+  :config
+  (ivy-rich-mode 1))
+
+;; Multiple cursors
+(use-package multiple-cursors
+  :bind (("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-c C-<" . mc/mark-all-like-this)))
+
+;; Flycheck: Syntax checking
+(use-package flycheck
+  :hook (prog-mode . flycheck-mode)
+  :config
+  (setq flycheck-check-syntax-automatically '(save mode-enabled)))
+
+;; Git gutter: Show git diff in margin
+(use-package git-gutter
+  :demand
+  :config
+  (global-git-gutter-mode +1)
+  (setq git-gutter:update-interval 0.5))
+
+;; Rainbow delimiters: Colorize matching parentheses
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Treemacs: File explorer sidebar
+(use-package treemacs
+  :bind (("C-x t t" . treemacs)
+         ("C-x t 1" . treemacs-select-window))
+  :config
+  (setq treemacs-width 30
+        treemacs-follow-mode t
+        treemacs-filewatch-mode t
+        treemacs-is-never-other-window t))
+
+(use-package treemacs-projectile
+  :after (treemacs projectile))
+
+(use-package treemacs-magit
+  :after (treemacs magit))
+
+;; vterm: Fast terminal emulator
+(use-package vterm
+  :bind ("C-c t" . vterm))
+
+;; Emmet: HTML/CSS abbreviation expansion
+(use-package emmet-mode
+  :hook ((html-mode css-mode sgml-mode web-mode typescript-mode) . emmet-mode)
+  :config
+  (setq emmet-move-cursor-between-quotes t))
+
+
+(add-to-list 'load-path (file-name-concat user-emacs-directory "lisp"))
+(require 'lsp-languages)
---
+++ emacs/.emacs.d/lisp/lsp-languages.el
@@ -0,0 +1,142 @@
+;;; lsp-languages.el --- LSP support for multiple languages -*- lexical-binding: t; -*-
+
+;; Ensure Mason and cargo binaries are in exec-path
+(use-package emacs
+  :config
+  (add-to-list 'exec-path (expand-file-name "~/.local/share/nvim/mason/bin"))
+  (add-to-list 'exec-path (expand-file-name "~/.cargo/bin"))
+  (setenv "PATH" (concat (expand-file-name "~/.local/share/nvim/mason/bin") ":"
+                         (expand-file-name "~/.cargo/bin") ":"
+                         (getenv "PATH"))))
+
+;; Completion framework
+(use-package company
+  :demand
+  :hook (prog-mode . company-mode)
+  :bind (:map company-active-map
+              ("C-n" . company-select-next)
+              ("C-p" . company-select-previous)
+              ("<tab>" . company-complete-selection)
+              ("TAB" . company-complete-selection))
+  :config
+  (setq company-idle-delay 0.1              ;; Show suggestions quickly
+        company-minimum-prefix-length 1     ;; Start completing after 1 char
+        company-selection-wrap-around t
+        company-tooltip-align-annotations t ;; Align annotations to the right
+        company-backends '(company-capf))   ;; Use completion-at-point (LSP)
+  (global-company-mode 1))                  ;; Enable globally
+
+;; Icons for completion items
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode)
+  :config
+  (setq company-box-icons-alist 'company-box-icons-all-the-icons
+        company-box-max-candidates 50
+        company-box-doc-enable t))
+
+;; Auto-pair brackets, parens, quotes
+(use-package emacs
+  :ensure nil
+  :hook (prog-mode . electric-pair-mode))
+
+;; Core LSP
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")    ;; optional LSP key prefix
+  :config
+  (setq lsp-enable-snippet t
+        lsp-prefer-flymake nil        ;; use flycheck if installed
+        lsp-headerline-breadcrumb-enable t
+        ;; Better completion experience
+        lsp-completion-provider :capf
+        lsp-completion-show-detail t
+        lsp-completion-show-kind t
+        lsp-signature-auto-activate t
+        lsp-signature-render-documentation t
+        ;; Performance tuning
+        lsp-idle-delay 0.2
+        lsp-log-io nil
+        ;; Use existing LSP servers from Mason/system PATH
+        lsp-rust-analyzer-server-command '("rust-analyzer")
+        lsp-gopls-server-path "gopls"
+        ;; Prevent automatic installation
+        lsp-enable-suggest-server-download nil
+        lsp-auto-guess-root t))
+
+;; UI enhancements for LSP
+(use-package lsp-ui
+  :after lsp-mode
+  :commands lsp-ui-mode
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (setq lsp-ui-doc-enable t
+        lsp-ui-doc-delay 0.3
+        lsp-ui-doc-position 'at-point
+        lsp-ui-doc-show-with-cursor t
+        lsp-ui-sideline-enable t
+        lsp-ui-sideline-show-hover t
+        lsp-ui-sideline-show-code-actions t
+        lsp-ui-sideline-show-diagnostics t
+        lsp-ui-peek-enable t
+        lsp-ui-peek-always-show t))
+
+;; Snippet support for better completions
+(use-package yasnippet
+  :demand
+  :hook (prog-mode . yas-minor-mode)
+  :config
+  (yas-reload-all))
+
+(use-package yasnippet-snippets
+  :after yasnippet)
+
+;; Optional: ivy integration for workspace symbols
+(use-package lsp-ivy
+  :after lsp
+  :commands lsp-ivy-workspace-symbol)
+
+;; === Language-specific hooks ===
+
+;; Python
+(use-package python
+  :hook (python-mode . lsp-deferred))
+
+;; JavaScript / JSX
+(use-package js
+  :hook ((js-mode js2-mode) . lsp-deferred))
+
+;; TypeScript / TSX
+(use-package typescript-mode
+  :mode ("\\.ts\\'" "\\.tsx\\'")
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
+
+;; Rust
+(use-package rust-mode
+  :hook (rust-mode . lsp-deferred)
+  :config
+  ;; Use LSP for checking, disable cargo checker
+  (setq rust-format-on-save nil)
+  (with-eval-after-load 'flycheck
+    (setq-default flycheck-disabled-checkers '(rust-cargo rust-clippy))))
+
+;; Go
+(use-package go-mode
+  :hook (go-mode . lsp-deferred))
+
+;; C / C++
+(use-package cc-mode
+  :hook ((c-mode c++-mode) . lsp-deferred)
+  :config
+  (setq c-default-style "linux"
+        c-basic-offset 2))
+
+
+(provide 'lsp-languages)
+;;; lsp-languages.el ends here
+
