ccf750f1005a7a068fd36a5b9abaaa060d32b6e7
David Muriithi <muriithid05@gmail.com>
2026-04-21 08:08:16 +0300

    fix bufferline and nvimtree

--- nvim/.config/nvim/lua/configs/lspsaga.lua
+++ nvim/.config/nvim/lua/configs/lspsaga.lua
@@ -1,37 +1,38 @@
 local M = {}
 
 M.setup = function()
-	require("lspsaga").setup({
-		ui = {
-			border = "rounded",
-			beacon = { enable = false },
-			lightbulb = { enable = false },
-		},
-		lightbulb = {
-			enable = false,
-			sign = false,
-		},
-		symbol_in_winbar = {
-			enable = true,
-			separator = " › ",
-			show_file = true,
-		},
-		code_action = {
-			extend_gitsigns = true,
-			keys = {
-				quit = { "q", "<ESC>" },
-				exec = "<CR>",
-			},
-		},
-		definition = {
-			keys = {
-				quit = "q",
-			},
-		},
-		hover = {
-			max_width = 0.9,
-		},
-	})
+  require("lspsaga").setup({
+    ui = {
+      border = "none",
+      beacon = { enable = false },
+      lightbulb = { enable = false },
+      winblend = 0,
+    },
+    lightbulb = {
+      enable = false,
+      sign = false,
+    },
+    symbol_in_winbar = {
+      enable = true,
+      separator = " › ",
+      show_file = true,
+    },
+    code_action = {
+      extend_gitsigns = true,
+      keys = {
+        quit = { "q", "<ESC>" },
+        exec = "<CR>",
+      },
+    },
+    definition = {
+      keys = {
+        quit = "q",
+      },
+    },
+    hover = {
+      max_width = 0.7,
+    },
+  })
 end
 
 return M
